59ca7bc48e418b8b6451b559450639235e6759a8
husnihnf <husnifaqih.kuliah@gmail.com>
2023-06-19 10:57:33 +0700

    modified:   application/views/welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -81,8 +81,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 		<p>alalal</p>
+<<<<<<< Updated upstream
 		<p>albar ganteng</p>
 		<p>rizki akbar</p>
+=======
+		<p>albarganteng</p>
+>>>>>>> Stashed changes
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
